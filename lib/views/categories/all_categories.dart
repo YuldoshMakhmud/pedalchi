852992fd91c4eab516f495e030147b9b097f3418
Yuldosh Makhmud <yuldoshmakhmud1@gmail.com>
2025-05-25 19:15:42 +0900

    all categories

--- lib/views/categories/all_categories.dart
+++ lib/views/categories/all_categories.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodchi/common/app_style.dart';
+import 'package:foodchi/common/back_ground_container.dart';
+import 'package:foodchi/common/reusable_text.dart';
+import 'package:foodchi/constants/constants.dart';
+import 'package:foodchi/constants/uidata.dart';
+import 'package:foodchi/views/categories/widget/category_tile.dart';
 
 class AllCategories extends StatelessWidget {
   const AllCategories({super.key});
@@ -7,7 +14,24 @@ class AllCategories extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('All Categories'),
+        elevation: 0,
+        backgroundColor: kOffWhite,
+        title: ReusableText(
+            text: "Categories", style: appStyle(12, kGray, FontWeight.w600)),
+      ),
+      body: BackGroundContainer(
+        color: Colors.white,
+        child: Container(
+          padding: EdgeInsets.only(left: 12.w, top: 10.h),
+          height: height,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: List.generate(categories.length, (i) {
+              var category = categories[i];
+              return CategoryTile(category: category);
+            }),
+          ),
+        ),
       ),
     );
   }
