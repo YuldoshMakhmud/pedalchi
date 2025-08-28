e28f892044c7c52a2a6b18835c10eb3f1d0894d4
unknown <yuldoshmakhmud1@gmail.com>
2025-08-28 16:19:24 +0900

    orders- carts

---
+++ lib/models/distance_time.dart
@@ -0,0 +1,11 @@
+class DistanceTime {
+  final double price;
+  final double distance;
+  final double time;
+
+  DistanceTime({
+    required this.price,
+    required this.distance,
+    required this.time,
+  });
+}
